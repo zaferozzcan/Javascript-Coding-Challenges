4ebbb9dc374fb85afaddb61f0ad2ae85a8f3fcbc
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-09-22 23:07:24 -0400

    Reverse String

---
+++ reverse-string.js
@@ -0,0 +1,40 @@
+function reverseString(str) {
+    var revString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        revString += str[i]
+    }
+    console.log(revString);
+}
+
+
+console.log(reverseString("hello"));
+
+
+
+// complete challenge with some built-in methods
+
+function reversedString(str) {
+    // we can take advantage of .reverse() method.Said that, this medhod only works 
+    // with array so turn the string into an array.
+    var strArr = str.split("");
+    // reverse the array
+    var reversedArr = strArr.reverse();
+    // concat the items of array
+    var concatReverseArr = reversedArr.join("")
+
+    return concatReverseArr
+}
+
+
+console.log(reversedString("hello"));
+
+
+
+// return it in one line of code 
+
+function reverser(str) {
+
+    return str.split("").reverse().join("");
+}
+
+console.log(reverser("hello"));
